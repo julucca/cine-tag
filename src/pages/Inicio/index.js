da6d660605b7ef8ce9a3cbcbfe5bc0ff44a2504f
Juliana Lucca <lucca.editora@gmail.com>
2023-05-16 16:08:18 -0300

    create card section

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,10 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 import React from "react";
 
 export default function Inicio() {
@@ -8,6 +12,14 @@ export default function Inicio() {
     <>
       <Cabecalho />
       <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />
+        })}
+      </section>
       <Rodape />
     </>
   );
