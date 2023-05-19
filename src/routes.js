03d2be6df677f56566d43e260693094ee016de07
Juliana Lucca <lucca.editora@gmail.com>
2023-05-19 15:03:09 -0300

    create base page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
-import Cabecalho from "components/Cabecalho";
-import Container from "components/Container";
-import Rodape from "components/Rodape";
-import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 import Player from "pages/Player";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -12,18 +9,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Container>
-        <FavoritosProvider>
-          <Routes>
-            <Route path="/" element={<Inicio />}></Route>
-            <Route path="/favoritos" element={<Favoritos />}></Route>
-            <Route path="/:id" element={<Player />}></Route>
-            <Route path="*" element={<NaoEncontrada />}></Route>
-          </Routes>
-        </FavoritosProvider>
-      </Container>
-      <Rodape />
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="favoritos" element={<Favoritos />}></Route>
+          <Route path=":id" element={<Player />}></Route>
+          <Route path="*" element={<NaoEncontrada />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
