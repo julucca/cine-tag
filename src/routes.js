239f2bdd48f26c0314c2ad407468ce3368cb7279
Juliana Lucca <lucca.editora@gmail.com>
2023-05-19 14:18:09 -0300

    create dynamic routes dor video pages

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Rodape from "components/Rodape";
 import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -16,6 +17,7 @@ export default function AppRoutes() {
           <Routes>
             <Route path="/" element={<Inicio />}></Route>
             <Route path="/favoritos" element={<Favoritos />}></Route>
+            <Route path="/:id" element={<Player />}></Route>
           </Routes>
         </FavoritosProvider>
       </Container>
